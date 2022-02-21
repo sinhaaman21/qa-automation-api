f4e4eabdc13554d9fc7488eae6deec224be1b4c3
Aman Sinha <aman.sinha@limechat.ai>
2022-02-21 16:48:09 +0530

    CreateEvent Test

--- cypress/support/constant.js
+++ cypress/support/constant.js
@@ -1,10 +1,12 @@
 const host = `https://stgapp.limechat.ai`;
+const hostLimeKit = `https://limekit-stgapp.limechat.ai`
 const accountsIdQA = 27;
 const conversationsId = `1`; 
 const dashboardApiAccessToken = `878d5e45be26a2010d7bbdf13bce4a7e3082804e78f862b1dd41572aabfd2ac1`;
 
 export default {
     host,
+    hostLimeKit,
     accountsIdQA,
     conversationsId,
     dashboardApiAccessToken
